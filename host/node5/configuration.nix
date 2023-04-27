27177434c54319e3207936c2a6c372474524462e
Göran Heinemann <goeran@karsdorf.net>
2023-04-27 17:19:44 +0200

    laptop config in flake

---
+++ host/node5/configuration.nix
@@ -0,0 +1,322 @@
+{ config, lib, pkgs, modulesPath, ... }:
+
+let
+  zfsRoot.partitionScheme = {
+    biosBoot = "-part5";
+    efiBoot = "-part1";
+    swap = "-part4";
+    bootPool = "-part2";
+    rootPool = "-part3";
+  };
+  zfsRoot.devNodes = "/dev/disk/by-id/"; # MUST have trailing slash! /dev/disk/by-id/
+  zfsRoot.bootDevices = (import ./machine.nix).bootDevices;
+  zfsRoot.mirroredEfi = "/boot/efis/";
+
+in {
+  imports = [
+    (modulesPath + "/installer/scan/not-detected.nix")
+      ./dconf.nix
+  ];
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  systemd.services.zfs-mount.enable = false;
+
+  networking.hostName = "node5";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/Berlin";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "de_DE.UTF-8";
+    LC_IDENTIFICATION = "de_DE.UTF-8";
+    LC_MEASUREMENT = "de_DE.UTF-8";
+    LC_MONETARY = "de_DE.UTF-8";
+    LC_NAME = "de_DE.UTF-8";
+    LC_NUMERIC = "de_DE.UTF-8";
+    LC_PAPER = "de_DE.UTF-8";
+    LC_TELEPHONE = "de_DE.UTF-8";
+    LC_TIME = "de_DE.UTF-8";
+  };
+
+  services.xserver.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.desktopManager.gnome.enable = true;
+  environment.gnome.excludePackages = with pkgs.gnome; [
+    baobab      # disk usage analyzer
+    cheese      # photo booth
+    #eog         # image viewer
+    #epiphany    # web browser
+    gedit       # text editor
+    simple-scan # document scanner
+    #totem       # video player
+    yelp        # help viewer
+    evince      # document viewer
+    geary       # email client
+
+    # these should be self explanatory
+    gnome-calculator gnome-calendar gnome-clocks gnome-contacts
+    gnome-font-viewer gnome-logs gnome-maps gnome-music
+    gnome-weather pkgs.gnome-connections
+  ];
+  services.xserver = {
+    layout = "de";
+    xkbVariant = "";
+  };
+
+  console.keyMap = "de";
+
+  services.printing.enable = false;
+
+  # Enable sound with pipewire.
+  sound.enable = true;
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  security.polkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    jack.enable = false;
+  };
+
+  users.users.goeranh = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "libvirtd" "docker" "networkmanager" "dialout" ];
+    packages = with pkgs; [
+	  bitwarden
+	  btrfs-progs
+	  chromium
+      dbeaver
+      discord
+      filezilla
+      firefox 
+      gajim 
+      ghidra
+      gitkraken
+      gnome3.gnome-terminal
+      gnome-builder
+      jetbrains.idea-community 
+      jetbrains.jdk
+      libreoffice
+      okular
+      poppler_utils 
+      quickemu
+      rambox
+      shotwell
+      signal-desktop 
+	  spotify
+	  tailscale
+      thunderbird 
+      vieb 
+      virt-manager
+      virt-viewer
+      vlc
+    ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  environment.systemPackages = with pkgs; [
+	  bash
+      bat 
+      cargo 
+	  clang
+	  clang-tools
+      cmake
+	  direnv
+      exa
+      fzf 
+	  gcc
+      gcc 
+      gef
+      gettext
+	  git
+      gnupg
+      gofu
+      htop 
+      iftop
+      ifuse
+      libimobiledevice
+	  libxcrypt
+      lm_sensors 
+      lsof 
+      meson
+	  neovim
+	  ninja
+      nix-direnv
+      nmap 
+      nodejs
+      rustc 
+      smartmontools
+	  tmux
+      unzip 
+      wget
+      zellij
+  ];
+
+  virtualisation.docker.enable = true;
+  virtualisation.virtualbox.host.enable = true;
+  virtualisation.libvirtd.enable = true;
+  programs.dconf.enable = true;
+  services.fwupd.enable = true;
+
+  programs.git.enable = true;
+  # networking.firewall.allowedTCPPorts = [ 22 ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  networking.firewall.enable = true;
+
+  system.copySystemConfiguration = true;
+
+  system.stateVersion = "22.11";
+
+
+  services = {
+    openssh.enable = false;
+    usbmuxd = {
+      enable = true;
+      package = pkgs.usbmuxd2;
+    };
+
+    tailscale.enable = true;
+    mysql = {
+      enable = true;
+      package = pkgs.mariadb;
+      ensureDatabases = [
+        "shop"
+        "datanature"
+      ];
+      ensureUsers = [
+        {
+          name = "goeranh";
+          ensurePermissions = {
+            "shop.*" = "ALL PRIVILEGES";
+            "datanature.*" = "ALL PRIVILEGES";
+          };
+        }
+      ];
+    };
+  };
+
+  home-manager.users.goeranh = { pkgs, ... }: {
+    home = {
+      stateVersion = "22.11";
+      file = {
+	  ".gitconfig".text = builtins.readFile /home/goeranh/projects/dotfiles/.gitconfig;
+	".config/nvim/" = {
+	  source = /home/goeranh/projects/nvim-config;
+	  recursive = true;
+	};
+	".goeranh.de".text = builtins.readFile /home/goeranh/projects/dotfiles/.goeranh.sh;
+	};
+    };
+    programs = {
+    };
+  };
+
+
+
+
+  boot.initrd.availableKernelModules = [
+    "ahci"
+    "xhci_pci"
+    "virtio_pci"
+    "virtio_blk"
+    "ehci_pci"
+    "nvme"
+    "uas"
+    "sd_mod"
+    "sr_mod"
+    "sdhci_pci"
+  ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  fileSystems = {
+    "/" = {
+      device = "rpool/nixos/root";
+      fsType = "zfs";
+      options = [ "X-mount.mkdir" ];
+    };
+
+    "/home" = {
+      device = "rpool/nixos/home";
+      fsType = "zfs";
+      options = [ "X-mount.mkdir" ];
+    };
+
+    "/var/lib" = {
+      device = "rpool/nixos/var/lib";
+      fsType = "zfs";
+      options = [ "X-mount.mkdir" ];
+    };
+
+    "/var/log" = {
+      device = "rpool/nixos/var/log";
+      fsType = "zfs";
+      options = [ "X-mount.mkdir" ];
+    };
+
+    "/boot" = {
+      device = "bpool/nixos/root";
+      fsType = "zfs";
+      options = [ "X-mount.mkdir" ];
+    };
+  } // (builtins.listToAttrs (map (diskName: {
+    name = zfsRoot.mirroredEfi + diskName + zfsRoot.partitionScheme.efiBoot;
+    value = {
+      device = zfsRoot.devNodes + diskName + zfsRoot.partitionScheme.efiBoot;
+      fsType = "vfat";
+      options = [
+        "x-systemd.idle-timeout=1min"
+        "x-systemd.automount"
+        "noauto"
+        "nofail"
+      ];
+    };
+  }) zfsRoot.bootDevices));
+
+  swapDevices = (map (diskName: {
+    device = zfsRoot.devNodes + diskName + zfsRoot.partitionScheme.swap;
+    discardPolicy = "both";
+    randomEncryption = {
+      enable = true;
+      allowDiscards = true;
+    };
+  }) zfsRoot.bootDevices);
+
+  networking.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.cpu.amd.updateMicrocode =
+    lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  boot.supportedFilesystems = [ "zfs" "ntfs" ];
+  networking.hostId = "2b34dd1b";
+  boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
+  boot.loader.efi.efiSysMountPoint = with builtins;
+    (zfsRoot.mirroredEfi + (head zfsRoot.bootDevices) + zfsRoot.partitionScheme.efiBoot);
+  boot.zfs.devNodes = zfsRoot.devNodes;
+  boot.loader.efi.canTouchEfiVariables = false;
+  boot.loader.generationsDir.copyKernels = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.copyKernels = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.zfsSupport = true;
+  boot.loader.grub.extraInstallCommands = with builtins;
+    (toString (map (diskName:
+      "cp -r " + config.boot.loader.efi.efiSysMountPoint + "/EFI" + " "
+      + zfsRoot.mirroredEfi + diskName + zfsRoot.partitionScheme.efiBoot + "\n")
+      (tail zfsRoot.bootDevices)));
+  boot.loader.grub.devices =
+    (map (diskName: zfsRoot.devNodes + diskName) zfsRoot.bootDevices);
+}
+
