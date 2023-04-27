d8c305ec1a482222c2e83c9950a5b7105ef2075d
Göran Heinemann <goeran@karsdorf.net>
2023-04-27 16:31:14 +0200

    disable sleep mode

--- host/desktop/default.nix
+++ host/desktop/default.nix
@@ -92,4 +92,9 @@
 
   system.stateVersion = "22.11"; # Did you read the comment?
 
+  systemd.targets.sleep.enable = false;
+  systemd.targets.suspend.enable = false;
+  systemd.targets.hibernate.enable = false;
+  systemd.targets.hybrid-sleep.enable = false;
+
 }
