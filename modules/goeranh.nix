e1baa18a0e32d81213e01e024f6cf8df20db5cf7
Göran Heinemann <goeran@karsdorf.net>
2023-05-01 12:26:35 +0200

    no list merge needed

--- modules/goeranh.nix
+++ modules/goeranh.nix
@@ -35,7 +35,7 @@
       ];
     };
 
-    environment.systemPackages = with pkgs; config.environment.systemPackages ++ [
+    environment.systemPackages = with pkgs; [
       bash
       bat
       direnv
