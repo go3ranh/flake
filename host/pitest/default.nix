0a79cd94a531f8a7487ed03d38ad8ee0dc6dc3b2
Göran Heinemann <goeran@karsdorf.net>
2023-10-08 18:53:36 +0200

    install vaultwarden

--- host/pitest/default.nix
+++ host/pitest/default.nix
@@ -156,8 +156,21 @@
             "/atuin/" = {
               proxyPass = "http://127.0.0.1:8888";
             };
+            "/vw/" = {
+              proxyPass = "http://127.0.0.1:8222";
+            };
           };
         };
+      };
+    };
+    vaultwarden = {
+      enable = true;
+      config = {
+        DOMAIN = "https://${config.networking.fqdn}/vw";
+        SIGNUPS_ALLOWED = true;
+        ROCKET_ADDRESS = "127.0.0.1";
+        ROCKET_PORT = 8222;
+        ROCKET_LOG = "critical";
       };
     };
   };
