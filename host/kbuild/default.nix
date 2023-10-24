1d27d35a3b1967ffa82d8b172e8b8aa9f1b415ce
Göran Heinemann <goeran@karsdorf.net>
2023-10-24 19:51:51 +0200

    kbuild build arm packages emulated

--- host/kbuild/default.nix
+++ host/kbuild/default.nix
@@ -3,6 +3,7 @@
 {
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "virtio_pci" "virtio_scsi" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
