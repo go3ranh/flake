ad990329bbfb049970f3c5ea3ed1e02cac0712f7
Göran Heinemann <goeran@karsdorf.net>
2023-11-14 03:32:54 +0100

    vim completion and undotree

--- packages.nix
+++ packages.nix
@@ -76,6 +76,7 @@
           vim-floaterm
           nvim-web-devicons
           nvim-tree-lua
+          undotree
         ];
 
         pack = archpkgs.pkgs.linkFarm "neovim-plugins"
@@ -107,6 +108,62 @@
                             })
 
                             local cmp = require('cmp')
+														cmp.setup {
+
+															mapping = {
+
+																-- ... Your other configuration ...
+
+																['<C-Space>'] = cmp.mapping.confirm {
+																	behavior = cmp.ConfirmBehavior.Insert,
+																	select = true,
+																},
+
+																['<Tab>'] = function(fallback)
+																	if not cmp.select_next_item() then
+																		if vim.bo.buftype ~= 'prompt' and has_words_before() then
+																			cmp.complete()
+																		else
+																			fallback()
+																		end
+																	end
+																end,
+
+																['<S-Tab>'] = function(fallback)
+																	if not cmp.select_prev_item() then
+																		if vim.bo.buftype ~= 'prompt' and has_words_before() then
+																			cmp.complete()
+																		else
+																			fallback()
+																		end
+																	end
+																end,
+															},
+
+															snippet = {
+																-- We recommend using *actual* snippet engine.
+																-- It's a simple implementation so it might not work in some of the cases.
+																expand = function(args)
+																	unpack = unpack or table.unpack
+																	local line_num, col = unpack(vim.api.nvim_win_get_cursor(0))
+																	local line_text = vim.api.nvim_buf_get_lines(0, line_num - 1, line_num, true)[1]
+																	local indent = string.match(line_text, '^%s*')
+																	local replace = vim.split(args.body, '\n', true)
+																	local surround = string.match(line_text, '%S.*') or "" 
+																	local surround_end = surround:sub(col)
+
+																	replace[1] = surround:sub(0, col - 1)..replace[1]
+																	replace[#replace] = replace[#replace]..(#surround_end > 1 and ' ' or "")..surround_end
+																	if indent ~= "" then
+																		for i, line in ipairs(replace) do
+																			replace[i] = indent..line
+																		end
+																	end
+
+																	vim.api.nvim_buf_set_lines(0, line_num - 1, line_num, true, replace)
+																end,
+															},
+														}
                             local cmp_select = {behavior = cmp.SelectBehavior.Select}
                             local cmp_mappings = lsp.defaults.cmp_mappings({
                               ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
