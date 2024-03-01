b44d449c9805056623e2b7a37d5114532009618b
Goeran Heinemann <goeran@karsdorf.net>
2024-03-01 08:43:52 +0100

    workstation is x86_64

--- flake.nix
+++ flake.nix
@@ -131,8 +131,8 @@
                 self.packages.aarch64-linux.customvim
               ];
               programs.bash.interactiveShellInit = ''
-                source ${self.packages.aarch64-linux.settings.bashrc.outPath}
-                source ${self.packages.aarch64-linux.settings.goeranh.outPath}
+								source ${self.packages.x86_64-linux.settings.bashrc.outPath}
+								source ${self.packages.x86_64-linux.settings.goeranh.outPath}
               '';
             }
             self.nixosModules.goeranh
