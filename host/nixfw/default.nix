d01bc9c3926ae3bed6d2f21d62dff5d577920973
Goeran Heinemann <goeran@karsdorf.net>
2024-05-08 14:23:22 +0200

    librenms, nftables and sops

--- host/nixfw/default.nix
+++ host/nixfw/default.nix
@@ -1,6 +1,19 @@
 { config, pkgs, lib, ... }:
 
 {
+  sops = {
+    # This will automatically import SSH keys as age keys
+    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+    defaultSopsFile = ./secrets.yaml;
+    defaultSopsFormat = "yaml";
+    secrets = {
+      "dbpass" = {
+        owner = "librenms";
+        group = "librenms";
+        mode = "0440";
+      };
+    };
+  };
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 
   fileSystems."/" =
@@ -35,6 +48,57 @@
     nftables = {
       enable = true;
       ruleset = ''
+			  table inet filter {
+					chain input {
+						type filter hook input priority 0;
+						iifname lo accept
+						ct state {established, related} accept
+						ip saddr { 10.200.0.2, 10.0.0.0/24, 10.0.1.0/24 } ip protocol icmp counter accept
+						ip saddr { 10.200.0.2 } tcp dport { 22, 80, 443 } counter accept
+						ip daddr { 10.16.23.95 } tcp dport 22 accept
+						ip daddr { 10.0.0.1, 10.200.0.5, 10.220.0.1 } udp dport { 53 } accept
+						ip daddr { 10.0.0.0/24, 10.0.1.0/24, 10.200.0.0/24, 10.220.0.0/24 } tcp dport { 53 } accept
+
+						counter drop
+					}
+
+					chain output {
+						type filter hook output priority 0;
+						accept
+					}
+
+					chain wireguard-to-lan {
+						#ip saddr 10.200.0.2 ip daddr { 10.200.0.0/24, 10.0.0.0/24, 10.0.1.0/24 } tcp dport { 22, 80, 443 } counter accept
+						#ip saddr 10.200.0.2 ip daddr { 10.200.0.0/24, 10.0.0.0/24, 10.0.1.0/24 } ip protocol { tcp, udb, icmp } counter accept
+						ip saddr 10.200.0.2 ip daddr { 10.200.0.0/24, 10.0.0.0/24, 10.0.1.0/24 } ip protocol icmp counter accept
+						ip saddr 10.200.0.7 ip daddr { 10.0.0.132 } ip protocol tcp counter accept
+						counter drop
+					}
+
+					chain lan-outbound {
+						ip saddr { 10.0.0.0/24 } tcp dport { 80, 443 } counter accept
+						counter drop
+					}
+
+					chain lan-to-lan {
+						ip saddr { 10.0.0.0/24, 10.0.1.0/24 } ip daddr {10.0.0.0/24, 10.0.1.0/24 } tcp dport { 80, 443 } counter accept
+						ip saddr { 10.0.0.0/24, 10.0.1.0/24 } ip daddr {10.0.0.0/24, 10.0.1.0/24 } ip protocol icmp counter accept
+						counter drop
+					}
+
+					chain forward {
+						type filter hook forward priority 0;
+						ct state {established, related} accept
+						iifname "wg0" oifname { "ens18", "ens19", "wg1" } jump wireguard-to-lan
+						iifname "ens19" oifname { "ens18" } jump lan-outbound
+						iifname "wg1" oifname { "ens19" } jump lan-to-lan
+						iifname "ens19" oifname { "wg1" } jump lan-to-lan
+
+						ip saddr { 10.0.0.0/24 } ip daddr { 10.16.23.1 } accept
+						iifname "ens18" counter drop
+						counter accept
+					}
+				}
         table ip nat {
           chain PREROUTING {
             type nat hook prerouting priority dstnat; policy accept;
@@ -43,7 +107,7 @@
       '';
     };
     firewall = {
-      enable = true;
+      enable = lib.mkForce false;
       interfaces = {
         ens18 = {
 					allowedUDPPorts = [ 51820 ];
@@ -125,6 +189,13 @@
         			allow-recursion { any; };
         			'';
     };
+		librenms = {
+			enable = true;
+			database = {
+				passwordFile = "/run/secrets/dbpass";
+				createLocally = true;
+			};
+		};
   };
 
   systemd = {
@@ -225,6 +296,34 @@
         };
       };
     };
+		services = {
+			snmpd = 
+			let
+			  snmpd-config = pkgs.writeText "snmpd.conf" ''
+				  rocommunity test123 127.0.0.1/32
+				  com2sec notConfigUser  default       public
+				  group   notConfigGroup v2c           notConfigUser
+				  access  notConfigGroup ""      any       noauth    exact  systemview none  none
+				  view    systemview    included   .1.3.6.1.2.1.1
+					view    systemview    included   .1.3.6.1.2.1.25.1.1
+					sysLocation Bladeserver, Keller, Karsdorf
+					sysContact goeran@karsdorf.net
+					sysName ${config.networking.fqdn}
+				'';
+			in{
+				enable = true;
+				wantedBy = [ "multiuser.target" ];
+				description = "SNMP agent";
+				after = [ "network.target" ];
+				restartIfChanged = true;
+				serviceConfig = {
+					User = "root";
+					Group = "root";
+					Restart = "always";
+					ExecStart = "${pkgs.net-snmp}/bin/snmpd -Lf /var/log/snmpd.log -c ${snmpd-config.outPath}";
+				};
+			};
+		};
   };
 
   system.stateVersion = "23.11";
