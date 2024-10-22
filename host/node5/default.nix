203373656ea59f72b758d66764eab6de4cc1886a
Goeran Heinemann <{ID}+{username}@users.noreply.github.com>
2024-10-22 16:21:12 +0200

    add nh program

--- host/node5/default.nix
+++ host/node5/default.nix
@@ -254,6 +254,12 @@ in
 
   virtualisation.libvirtd.enable = true;
   programs.dconf.enable = true;
+  programs.nh = {
+    enable = true;
+    clean.enable = true;
+    clean.extraArgs = "--keep-since 4d --keep 3";
+    flake = "/home/goeranh/gitprojects/src/gitlab.goeranh.de/goeranh/flakeathome";
+  };
 
   programs.git.enable = true;
 
